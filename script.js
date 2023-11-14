27939cc4315aa06a2479c8ef9fcf54f34eca9113
El Zee <53586135+itzlawyo@users.noreply.github.com>
2023-11-13 21:06:08 -0500

    make changes

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
 /*----- constants -----*/
-const billAmount = document.querySelector('#billAmount');
-const tipKey = document.querySelector('.tip_keys');
-const amountOfPeople = document.querySelector('.numPeopleSlider');
-const customTip = document.querySelector('.tip_key--custom');
-let output = document.querySelector('.amountPerPerson_output');
+const tipKey = document.querySelectorAll(".tip_key");
+const customTip = document.querySelector("#customTipValue");
+const amountOfPeople = document.querySelector(".numPeopleSlider");
+const resetBtn = document.querySelector(".resetBtn");
+let billAmount = document.querySelector("#billAmount");
+let tipAmt = document.querySelector('#tip-amount');
+let totalAmt = document.querySelector("#total-amount");
+let output = document.querySelector(".amountPerPerson_output");
+let numPeopleAmt = document.querySelector("#split-num");
 /*----- app's state (variables) -----*/
 let tipAmount = 0;
-/*----- cached element references -----*/
+
 /*----- event listeners -----*/
-tipKey.addEventListener('click', function(e) {
-    tipAmount = e.target.id;
+// add an event listener for every tip key
+tipKey.forEach(key => {
+    key.addEventListener('click', (evt) => {
+        handleKeyClick(evt);
+    })
+})
+
+// add event listener on the custom tip amount
+customTip.addEventListener('change', function(e) {
+    tipAmount = (customTip.value/100);
     calculateAmtPerPerson();
+    console.log(tipAmount);
 });
 
-customTip.addEventListener('click', function(e) {
-    console.log(e);
-});
-
+// add event listener to detect a change in the number of people
 amountOfPeople.addEventListener('mouseup', function(e) {
     calculateAmtPerPerson();
 });
 
+// add event listener to the reset button
+resetBtn.addEventListener('click', function(e) {
+    tipAmount = 0;
+    billAmount.value = 0;
+    numPeopleAmt.value = 0;
+    calculateAmtPerPerson();
+});
+
 /*----- functions -----*/
+function handleKeyClick(e) {
+    tipAmount = e.target.id;
+    calculateAmtPerPerson();
+}
+
 function calculateAmtPerPerson() {
-    let totalAfterTip = (parseFloat(tipAmount) + 1) * parseFloat(billAmount.value);
-    let amtPerPerson = totalAfterTip / parseFloat(amountOfPeople.value);
-    output.innerText = `${amtPerPerson.toFixed(2)} / person`;
+    let bill = parseFloat(billAmount.value);
+    let tipPercent = parseFloat(tipAmount);
+    let numPeople = parseFloat(amountOfPeople.value);
+    let totalTip = bill * tipPercent;
+    let totalAfterTip = (tipPercent + 1) * bill;
+    let amtPerPerson = totalAfterTip / numPeople;
+    
+    output.textContent = `$${amtPerPerson.toFixed(2)}`;
+    tipAmt.textContent = `$${totalTip}`;
+    totalAmt.textContent = `$${totalAfterTip}`;
+    numPeopleAmt.textContent = `${numPeople}`;
 }
 
